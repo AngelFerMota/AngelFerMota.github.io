927abc3d259505881c4670cf008927e4f14745de
Angel <angelfernandezmota@gmail.com>
2025-09-08 13:52:07 +0200

    Browser

--- CvAngular20/src/assets/data/models.ts
+++
@@ -1,6 +0,0 @@
-export interface Skill { name:string; level:number; }
-export interface Experience {
-  role:string; company:string; from:string; to?:string; notes?:string;
-}
-export interface DownloadLink { label:string; url:string; }
-export interface SocialLink { label:string; url:string; icon:string; }
